3282bee297f1408ef95a22a0e4fe877e3ab758ad
rneal-home <r_rneal@hotmail.com>
2025-01-18 11:31:00 -0800

    changed a typo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on t eh Feature Branch
+          I was changed on the Feature Branch
         </a>
       </header>
     </div>
